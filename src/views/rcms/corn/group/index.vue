cc09ba19d71d8ff473f37b84dde11cba4f7c6c6c
CJ.Chen <imaxstone@outlook.com>
2024-11-23 16:10:00 +0800

    模版实现

--- src/views/rcms/corn/group/index.vue
+++ src/views/rcms/corn/group/index.vue
@@ -1,50 +1,52 @@
 <template>
-  <div class="rcms-plus-page">
-    <el-card>
-      <PlusSearch
-        v-model="searchForm"
-        :columns="searchColumns"
-        :show-number="3"
-        :col-props="{ xs: 1, sm: 1, md: 6, lg: 6, xl: 6 }"
-        :label-width="100"
-        @change="handleChange"
-        @search="handleSearch"
-        @reset="handleReset"
-      />
-    </el-card>
-    <el-card>
-      <PlusTable
-        :loadingStatus="loading"
-        table-layout="auto"
-        showOverflowTooltip
-        :columns="tableColumns"
-        :table-data="tableData"
-        :is-selection="true"
-        has-index-column
-        :index-table-column-props="{
-          label: '序号',
-          width: 60
-        }"
-        :adaptive="{
-          offsetBottom: 80
-        }"
-        :pagination="{
-          pageSizeList: pageSizeList,
-          total: total
-        }"
-        @paginationChange="handlePageChange"
-        @selection-change="handleSelectChange"
-      >
-        <template #title>
-          <el-button type="primary" plain :icon="Plus" @click="handleCreate"
-            >新增</el-button
-          >
-          <el-button type="danger" plain :icon="Delete" @click="handleDelete"
-            >删除</el-button
-          >
-        </template>
-      </PlusTable>
-    </el-card>
+  <div>
+    <div class="rcms-plus-page">
+      <el-card>
+        <PlusSearch
+          v-model="searchForm"
+          :columns="searchColumns"
+          :show-number="3"
+          :col-props="{ xs: 1, sm: 1, md: 6, lg: 6, xl: 6 }"
+          :label-width="100"
+          @change="handleChange"
+          @search="handleSearch"
+          @reset="handleReset"
+        />
+      </el-card>
+      <el-card>
+        <PlusTable
+          :loadingStatus="loading"
+          table-layout="auto"
+          showOverflowTooltip
+          :columns="tableColumns"
+          :table-data="tableData"
+          :is-selection="true"
+          has-index-column
+          :index-table-column-props="{
+            label: '序号',
+            width: 60
+          }"
+          :adaptive="{
+            offsetBottom: 80
+          }"
+          :pagination="{
+            pageSizeList: pageSizeList,
+            total: total
+          }"
+          @paginationChange="handlePageChange"
+          @selection-change="handleSelectChange"
+        >
+          <template #title>
+            <el-button type="primary" plain :icon="Plus" @click="handleCreate"
+              >新增</el-button
+            >
+            <el-button type="danger" plain :icon="Delete" @click="handleDelete"
+              >删除</el-button
+            >
+          </template>
+        </PlusTable>
+      </el-card>
+    </div>
     <PlusDialog
       v-model="show"
       :title="createTitle + '任务组'"
@@ -56,13 +58,14 @@
         <el-card>
           <PlusForm
             v-if="show"
+            ref="createFormRef"
             v-model="createForm"
+            labelWidth="110"
+            labelPosition="right"
             :columns="createColumns"
             :rules="createRules"
             footerAlign="center"
-            @change="handleDgChange"
             @submit="handleDgSubmit"
-            @reset="handleDgReset"
           >
             <template #footer="{ handleSubmit }">
               <div style="margin: 0 auto">
@@ -88,18 +91,12 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, reactive, toRefs } from "vue";
+import { ref, onMounted, reactive, toRefs, computed } from "vue";
 import { ElMessageBox } from "element-plus";
 import { useTable } from "plus-pro-components";
 import type { PageInfo, PlusColumn, FieldValues } from "plus-pro-components";
 import { Plus, Delete } from "@element-plus/icons-vue";
-import {
-  createRules,
-  createColumns,
-  searchColumns,
-  buildTableColum,
-  groupService
-} from "./utils/hook";
+import { searchColumns, buildTableColum, groupService } from "./utils/hook";
 
 // --- 查询条件区域 ---
 const searchForm = ref({});
@@ -186,18 +183,93 @@ onMounted(() => {
 // --- 编辑页面 ---
 const show = ref(false);
 const createLoading = ref(false);
-const createForm = {};
-const handleDgChange = (values: FieldValues, prop: PlusColumn) => {
-  console.log(values, prop, "change");
+const createForm = ref<FieldValues>({
+  isAuthorized: "N"
+});
+const createColumns: PlusColumn[] = [
+  {
+    label: "分组名称",
+    prop: "quartzGroupName"
+  },
+  {
+    label: "是否需要认证",
+    prop: "isAuthorized",
+    valueType: "select",
+    options: [
+      {
+        label: "是",
+        value: "Y"
+      },
+      {
+        label: "否",
+        value: "N"
+      }
+    ]
+  },
+  {
+    label: "返回值key",
+    prop: "authKey",
+    hideInForm: computed(() => createForm.value["isAuthorized"] === "N")
+  },
+  {
+    label: "请求类型",
+    prop: "requestType",
+    hideInForm: computed(() => createForm.value["isAuthorized"] === "N")
+  },
+  {
+    label: "请求地址",
+    prop: "requestPath",
+    hideInForm: computed(() => createForm.value["isAuthorized"] === "N")
+  },
+  {
+    label: "请求参数",
+    prop: "requestParams",
+    hideInForm: computed(() => createForm.value["isAuthorized"] === "N")
+  },
+  {
+    label: "请求头参数",
+    prop: "requestHeaders",
+    hideInForm: computed(() => createForm.value["isAuthorized"] === "N")
+  }
+];
+const createRules = {
+  quartzGroupName: [
+    {
+      required: true,
+      message: "请输入名称"
+    }
+  ],
+  isAuthorized: [
+    {
+      required: true,
+      message: "请确认是否需要认证"
+    }
+  ],
+  authKey: [
+    {
+      required: true,
+      message: "请输入返回值key"
+    }
+  ],
+  requestType: [
+    {
+      required: true,
+      message: "请输入请求类型"
+    }
+  ],
+  requestPath: [
+    {
+      required: true,
+      message: "请输入请求地址"
+    }
+  ]
 };
 const handleDgSubmit = (values: FieldValues) => {
   console.log(values, "Submit");
   createLoading.value = true;
 };
-const handleDgReset = () => {
-  console.log("handleReset");
-};
 const handleClose = () => {
+  createForm.value = { isAuthorized: "N" };
   show.value = false;
 };
 </script>
